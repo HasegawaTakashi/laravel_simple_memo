07dd837ae8174c880b074aa298f227fe842d6429
github takashi-hasegawa6 <euphonium813@gmail.com>
2022-01-21 20:12:06 +0900

    ログイン機能、メモ新規作成、一覧表示機能の実装
    ログイン機能
      -> app/Http/Controllers/Auth/
    メモ新規作成
      -> resources/views/create.blade.php
    一覧表示機能
      -> resources/views/layouts/app.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // メモデータを取得
+        $memos = Memo::select('memos.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC') // DESC->降順 ASC->昇順
+            ->get();
+
+        return view('create', compact('memos'));
+    }
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        // dump die -> メソッドの引数のとった値を質問して止める -> データ確認、デバック関数
+        // dd(\Auth::id());
+
+        Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+
+        return redirect(route('home'));
     }
 }
