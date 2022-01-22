d91ec92a7ae5a787cda48c41e307f7fdbcc397e2
github takashi-hasegawa6 <euphonium813@gmail.com>
2022-01-22 12:20:49 +0900

    作業途中

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,6 +33,7 @@ class HomeController extends Controller
 
         return view('create', compact('memos'));
     }
+
     public function store(Request $request)
     {
         $posts = $request->all();
@@ -40,6 +41,29 @@ class HomeController extends Controller
         // dd(\Auth::id());
 
         Memo::insert(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+
+        return redirect(route('home'));
+    }
+
+    public function edit($id)
+    {
+        // メモデータを取得
+        $memos = Memo::select('memos.*')
+        ->where('user_id', '=', \Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('updated_at', 'DESC') // DESC->降順 ASC->昇順
+        ->get();
+
+        $edit_memo = Memo::find($id);
+
+        return view('edit', compact('memos', 'edit_memo'));
+    }
+
+    public function update(Request $request)
+    {
+        $posts = $request->all();
+
+        Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
 
         return redirect(route('home'));
     }
