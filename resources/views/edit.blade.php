6740348fc119f6c5830ded5876c5d7031be7f483
github takashi-hasegawa6 <euphonium813@gmail.com>
2022-01-26 16:00:21 +0900

    5 メモとタグを紐づけて表示
    表示されたメモにタグが付いていた場合、タグも表示させる

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,18 +5,27 @@
         メモ編集
         <form class="card-body" action="{{route('destroy')}}" method="POST">
             @csrf
-            <input type="hidden" name="memo_id" value="{{$edit_memo['id']}}">
+            <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
             <button type="submit">削除</button>
         </form>
     </div>
     <form class="card-body" action="{{route('update')}}" method="POST">
         @csrf
-        <input type="hidden" name="memo_id" value="{{$edit_memo['id']}}">
+        <input type="hidden" name="memo_id" value="{{$edit_memo[0]['id']}}">
         <div class="form-group">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">
-                {{$edit_memo['content']}}
+            {{$edit_memo[0]['content']}}
             </textarea>
         </div>
+        @foreach($tags as $tag)
+        <div class="form-check form-check-inline mb-3">
+            <input type="checkbox" class="form-check-input" name="tags[]" id="{{$tag['id']}}" value="{{$tag['id']}}" {{in_array($tag['id'], $include_tags) ? 'checked' : ''}}>
+            <label for="{{$tag['id']}}" class="form-check-label">
+                {{$tag['name']}}
+            </label>
+        </div>
+        @endforeach
+        <input type="text" class="form-control w-50 mt-3 mb-2" name="new_tag" placeholder="新しいタグを入力">
         <button type="submit" class="btn btn-primary mt-3">更新</button>
     </form>
 </div>
