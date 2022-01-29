637d78611f139367e86766a987ac55a1d001ba91
github takashi-hasegawa6 <euphonium813@gmail.com>
2022-01-29 15:25:38 +0900

    12 追加CSSで全体のレイアウトを整える
    public/css/layout.cssを作成し、レイアウトを整える
      各カラムの余白を画面最大まで拡大
      一覧に長文があった場合、表示を省略させる
      一覧の上下余白をme-2とする

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,21 +2,21 @@
 @section('content')
 <div class="card">
     <div class="card-header">新規メモ作成</div>
-    <form class="card-body" action="{{route('store')}}" method="POST">
+    <form class="card-body my-card-body" action="{{route('store')}}" method="POST">
         @csrf
         <div class="form-group">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
         </div>
         @error('content')
-            <div class='alert alert-danger mt-2 mb-2'>メモ内容を入力してください</div>
+        <div class='alert alert-danger mt-2 mb-2'>メモ内容を入力してください</div>
         @enderror
         @foreach($tags as $tag)
-            <div class="form-check form-check-inline mb-3">
-                <input type="checkbox" class="form-check-input" name="tags[]" id="{{$tag['id']}}" value="{{$tag['id']}}">
-                <label for="{{$tag['id']}}" class="form-check-label">
-                    {{$tag['name']}}
-                </label>
-            </div>
+        <div class="form-check form-check-inline mb-3">
+            <input type="checkbox" class="form-check-input" name="tags[]" id="{{$tag['id']}}" value="{{$tag['id']}}">
+            <label for="{{$tag['id']}}" class="form-check-label">
+                {{$tag['name']}}
+            </label>
+        </div>
         @endforeach
         <input type="text" class="form-control w-50 mt-3 mb-2" name="new_tag" placeholder="新しいタグを入力">
         <button type="submit" class="btn btn-primary mt-3">保存</button>
